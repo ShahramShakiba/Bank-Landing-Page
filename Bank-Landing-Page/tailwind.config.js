8a04f79ea35c5a413051700e9e5dd93dba3d84c4
Shahram Shakiba <ShahramShakiba88@gmail.com>
2023-11-27 13:26:37 +0330

    Initializing the Bank-Landing-Page Project

---
+++ Bank-Landing-Page/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        'color-primary': '#01051e',
+        'color-primary-light': '#020726',
+        'color-primary-dark': '#010417',
+        'color-secondary': '#ff7d3b',
+        'color-gray': '#333',
+        'color-white': '#fff',
+        'color-blob': '#A427DF',
+      },
+    },
+  },
+  plugins: [],
+};
