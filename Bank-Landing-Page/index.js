78f0362b8b8ea800d9429a8e529fc2b0b9e738df
Shahram Shakiba <ShahramShakiba88@gmail.com>
2023-11-27 20:58:04 +0330

    Designing Hamburger-menu | add functionality

---
+++ Bank-Landing-Page/index.js
@@ -0,0 +1,19 @@
+const hamburgerIcon = document.querySelector('.hamburger'),
+  navList = document.querySelector('#menu'),
+  navLinks = document.querySelectorAll('#menu a'),
+  closeIcon = document.querySelector('.close');
+
+hamburgerIcon.addEventListener('click', () => {
+  navList.classList.toggle('hidden');
+});
+
+closeIcon.addEventListener('click', () => {
+  navList.classList.add('hidden');
+});
+
+// Close the menu when a link is clicked
+navLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    navList.classList.add('hidden');
+  });
+});
