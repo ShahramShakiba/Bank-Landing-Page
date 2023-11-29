99dbe41c37278ea70cbdc964a56fb08dd04a389f
Shahram Shakiba <ShahramShakiba88@gmail.com>
2023-11-29 16:41:09 +0330

    Fixing blobs of Header

--- Bank-Landing-Page/tailwind.config.js
+++ Bank-Landing-Page/tailwind.config.js
@@ -8,7 +8,6 @@ module.exports = {
         'color-primary': '#01051e',
         'color-primary-light': '#020726',
         'color-primary-dark': '#010417',
-        // 'color-secondary': '#ff7d3b',
         'color-gray': '#333',
         'color-blob': '#A427DF',
       },
